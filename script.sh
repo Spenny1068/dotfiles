e3f8be890324ae3815e47cc8e53e58f2f76db50a
Spenny1068 <spncrlll838@gmail.com>
2021-03-23 17:27:29 -0700

    can actually use this now :^)

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
 
 # install fzf
-sudo apt-get install fzf;    # APT (Debian/Ubuntu)
+# sudo apt-get install fzf;    # APT (Debian/Ubuntu)
 # choco install fzf;           # windows
 # brew install fzf;            # mac
